f7184c83d294008d745b5b7ea7e5ec0710f705a8
liuchaoren <chaorenkindle@gmail.com>
2020-09-09 11:57:04 -0400

    Fix the signature of AuthorizedHttp.request to match the signature of httplib2.Http.request

--- tests/test_google_auth_httplib2.py
+++ tests/test_google_auth_httplib2.py
@@ -22,18 +22,27 @@ from tests import compliance
 
 
 class MockHttp(object):
+
     def __init__(self, responses, headers=None):
         self.responses = responses
         self.requests = []
         self.headers = headers or {}
         self.add_certificate = mock.Mock(return_value=None)
 
-    def request(self, url, method='GET', body=None, headers=None, **kwargs):
-        self.requests.append((method, url, body, headers, kwargs))
+    def request(self,
+                url,
+                method='GET',
+                body=None,
+                headers=None,
+                redirections=httplib2.DEFAULT_MAX_REDIRECTS,
+                connection_type=None):
+        self.requests.append(
+            (method, url, body, headers, redirections, connection_type))
         return self.responses.pop(0)
 
 
 class MockResponse(object):
+
     def __init__(self, status=http_client.OK, data=b''):
         self.status = status
         self.data = data
@@ -44,6 +53,7 @@ class MockResponse(object):
 
 
 class TestRequestResponse(compliance.RequestResponseTests):
+
     def make_request(self):
         http = httplib2.Http()
         return google_auth_httplib2.Request(http)
@@ -54,8 +64,7 @@ class TestRequestResponse(compliance.RequestResponseTests):
         request = google_auth_httplib2.Request(http)
         request(url=url, method='GET', timeout=5)
 
-        assert http.requests[0] == (
-            'GET', url, None, None, {})
+        assert http.requests[0] == ('GET', url, None, None, {})
 
 
 def test__make_default_http():
@@ -64,6 +73,7 @@ def test__make_default_http():
 
 
 class MockCredentials(object):
+
     def __init__(self, token='token'):
         self.token = token
 
@@ -98,8 +108,7 @@ class TestAuthorizedHttp(object):
 
     def test_follow_redirects(self):
         auth_http = google_auth_httplib2.AuthorizedHttp(
-            mock.sentinel.credentials
-        )
+            mock.sentinel.credentials)
 
         assert auth_http.follow_redirects == auth_http.http.follow_redirects
 
@@ -109,8 +118,7 @@ class TestAuthorizedHttp(object):
 
     def test_timeout(self):
         authed_http = google_auth_httplib2.AuthorizedHttp(
-            mock.sentinel.credentials
-        )
+            mock.sentinel.credentials)
 
         assert authed_http.timeout == authed_http.http.timeout
 
@@ -119,8 +127,7 @@ class TestAuthorizedHttp(object):
 
     def test_redirect_codes(self):
         authed_http = google_auth_httplib2.AuthorizedHttp(
-            mock.sentinel.credentials
-        )
+            mock.sentinel.credentials)
 
         assert authed_http.redirect_codes == authed_http.http.redirect_codes
 
@@ -129,24 +136,22 @@ class TestAuthorizedHttp(object):
 
     def test_add_certificate(self):
         authed_http = google_auth_httplib2.AuthorizedHttp(
-            mock.sentinel.credentials,
-            http=MockHttp(MockResponse())
-        )
+            mock.sentinel.credentials, http=MockHttp(MockResponse()))
 
-        authed_http.add_certificate(
-            "key", "cert", "domain", password="password"
-        )
+        authed_http.add_certificate("key",
+                                    "cert",
+                                    "domain",
+                                    password="password")
         authed_http.http.add_certificate.assert_called_once_with(
-            "key", "cert", "domain", password="password"
-        )
+            "key", "cert", "domain", password="password")
 
     def test_request_no_refresh(self):
         mock_credentials = mock.Mock(wraps=MockCredentials())
         mock_response = MockResponse()
         mock_http = MockHttp([mock_response])
 
-        authed_http = google_auth_httplib2.AuthorizedHttp(
-            mock_credentials, http=mock_http)
+        authed_http = google_auth_httplib2.AuthorizedHttp(mock_credentials,
+                                                          http=mock_http)
 
         response, data = authed_http.request(self.TEST_URL)
 
@@ -154,19 +159,20 @@ class TestAuthorizedHttp(object):
         assert data == mock_response.data
         assert mock_credentials.before_request.called
         assert not mock_credentials.refresh.called
-        assert mock_http.requests == [
-            ('GET', self.TEST_URL, None, {'authorization': 'token'}, {})]
+        assert mock_http.requests == [('GET', self.TEST_URL, None, {
+            'authorization': 'token'
+        }, httplib2.DEFAULT_MAX_REDIRECTS, None)]
 
     def test_request_refresh(self):
         mock_credentials = mock.Mock(wraps=MockCredentials())
         mock_final_response = MockResponse(status=http_client.OK)
         # First request will 401, second request will succeed.
         mock_http = MockHttp([
-            MockResponse(status=http_client.UNAUTHORIZED),
-            mock_final_response])
+            MockResponse(status=http_client.UNAUTHORIZED), mock_final_response
+        ])
 
-        authed_http = google_auth_httplib2.AuthorizedHttp(
-            mock_credentials, http=mock_http)
+        authed_http = google_auth_httplib2.AuthorizedHttp(mock_credentials,
+                                                          http=mock_http)
 
         response, data = authed_http.request(self.TEST_URL)
 
@@ -174,29 +180,56 @@ class TestAuthorizedHttp(object):
         assert data == mock_final_response.data
         assert mock_credentials.before_request.call_count == 2
         assert mock_credentials.refresh.called
-        assert mock_http.requests == [
-            ('GET', self.TEST_URL, None, {'authorization': 'token'}, {}),
-            ('GET', self.TEST_URL, None, {'authorization': 'token1'}, {})]
+        assert mock_http.requests == [('GET', self.TEST_URL, None, {
+            'authorization': 'token'
+        }, httplib2.DEFAULT_MAX_REDIRECTS, None),
+                                      ('GET', self.TEST_URL, None, {
+                                          'authorization': 'token1'
+                                      }, httplib2.DEFAULT_MAX_REDIRECTS, None)]
 
     def test_request_stream_body(self):
         mock_credentials = mock.Mock(wraps=MockCredentials())
         mock_response = MockResponse()
         # Refresh is needed to cover the resetting of the body position.
-        mock_http = MockHttp([
-            MockResponse(status=http_client.UNAUTHORIZED),
-            mock_response])
+        mock_http = MockHttp(
+            [MockResponse(status=http_client.UNAUTHORIZED), mock_response])
 
         body = six.StringIO('body')
         body.seek(1)
 
-        authed_http = google_auth_httplib2.AuthorizedHttp(
-            mock_credentials, http=mock_http)
+        authed_http = google_auth_httplib2.AuthorizedHttp(mock_credentials,
+                                                          http=mock_http)
 
-        response, data = authed_http.request(
-            self.TEST_URL, method='POST', body=body)
+        response, data = authed_http.request(self.TEST_URL,
+                                             method='POST',
+                                             body=body)
 
         assert response == mock_response
         assert data == mock_response.data
-        assert mock_http.requests == [
-            ('POST', self.TEST_URL, body, {'authorization': 'token'}, {}),
-            ('POST', self.TEST_URL, body, {'authorization': 'token1'}, {})]
+        assert mock_http.requests == [('POST', self.TEST_URL, body, {
+            'authorization': 'token'
+        }, httplib2.DEFAULT_MAX_REDIRECTS, None),
+                                      ('POST', self.TEST_URL, body, {
+                                          'authorization': 'token1'
+                                      }, httplib2.DEFAULT_MAX_REDIRECTS, None)]
+
+    def test_request_positional_args(self):
+        """Verifies that clients can pass args to request as positioanls."""
+        mock_credentials = mock.Mock(wraps=MockCredentials())
+        mock_response = MockResponse()
+        mock_http = MockHttp([mock_response])
+
+        authed_http = google_auth_httplib2.AuthorizedHttp(mock_credentials,
+                                                          http=mock_http)
+
+        response, data = authed_http.request(self.TEST_URL, 'GET', None, None,
+                                             httplib2.DEFAULT_MAX_REDIRECTS,
+                                             None)
+
+        assert response == mock_response
+        assert data == mock_response.data
+        assert mock_credentials.before_request.called
+        assert not mock_credentials.refresh.called
+        assert mock_http.requests == [('GET', self.TEST_URL, None, {
+            'authorization': 'token'
+        }, httplib2.DEFAULT_MAX_REDIRECTS, None)]
